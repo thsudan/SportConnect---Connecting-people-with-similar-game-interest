85338583e559cc47bed9c9b9bed1a02d6f96276f
Sudan Thapa <sudanthapa@Sudans-Air.lan>
2024-10-01 02:00:58 +1000

    initial test

--- server.js
+++ server.js
@@ -1,40 +1,10 @@
-const express = require('express');
+// server.js
 const http = require('http');
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const socketIo = require('socket.io');
-const session = require('express-session'); // Added for session management
-const userRoutes = require('./routes/userRoutes');
+const app = require('./app');  // Import your app
 
-const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
-const mongoURI = 'mongodb+srv://s224589292:ayQ8yTJGRyJF2hzL@cluster0.ctiaqgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0on_string';
-
-mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Session middleware
-app.use(session({
-  secret: 'your_secret_key', // Replace with a secure secret key
-  resave: false,
-  saveUninitialized: true,
-}));
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/', userRoutes);
-
-app.get('/search-based-on-location', (req, res) => {
-  res.render('locationSearch');
-});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -53,4 +23,4 @@ server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+module.exports = server;  // Export the server
--- test/test.js
+++ test/test.js
@@ -1,35 +1,24 @@
-// test/routes.test.js
-const request = require('supertest');
-const express = require('express');
-const app = require('../server'); // Import your Express app
+// test/test.js
 const chai = require('chai');
-const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const server = require('../server');  // Import the server
+const { expect } = chai;
 
-describe('GET /', () => {
-  it('Index page tested successfully', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end((err, res) => {
+chai.use(chaiHttp);
+
+describe('Search Functionality', function() {
+  after(function(done) {
+    server.close(done);  // Close the server after tests
+  });
+
+  it('should return search results for sports', function(done) {
+    chai.request(server)
+      .get('/search-by-sport')
+      .end(function(err, res) {
         if (err) return done(err);
-        expect(res.text).to.include('<title>SIT725- Task4.2P (Add a database)</title>');
-        expect(res.text).to.include('<h1>PARK YOUR VEHICLE</h1>');
+        expect(res).to.have.status(200);  // Check that the response has status 200
+        expect(res).to.be.html;  // Ensure the response is HTML
         done();
       });
   });
 });
-
-describe('GET /signup', () => {
-  it('Sign Up page tested successfully', (done) => {
-    request(app)
-      .get('/signup')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.include('<title>Registration Successful</title>');
-        expect(res.text).to.include('<h1>Congratulations!</h1>');
-        done();
-      });
-  });
-});
-
